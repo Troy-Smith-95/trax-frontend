ba0483bfe2909de91517bd200b4a6c2d8ff65c84
Troy-Smith-95 <trsmith.runner@gmail.com>
2022-12-07 23:58:04 -0500

    add path to link button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,9 +9,10 @@ function Hero() {
                 <h2 className='hero__tagline'>Powerful insights for music makers</h2>
                 <p className='hero__text'>See how the sound of your genre is changing, leverage insights to make music that grows your audience, find new music influences</p>
                 <p className='hero__cta'>Trax is FREE. Get started now!</p>
-                <Link className='hero__link'>Go to Dashboard</Link>
+                <Link to='/dashboard' className='hero__link'>Go to Dashboard</Link>
             </div>
             <img className='hero__img' src={heroImage} alt="Trax Dashboard" />
+
         </section>
     );
 }
