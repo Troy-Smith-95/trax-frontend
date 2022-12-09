f98256ce6b2daa7623e1556804191c4ad7ce5ea2
Troy-Smith-95 <trsmith.runner@gmail.com>
2022-12-09 16:58:44 -0500

    add api call to retrieve audio features for selected genre, display in progress bar

--- src/Components/AudioFeatureBar/AudioFeatureBar.jsx
+++ src/Components/AudioFeatureBar/AudioFeatureBar.jsx
@@ -2,14 +2,14 @@ import './AudioFeatureBar.scss';
 import ProgressBar from "@ramonak/react-progress-bar";
 import infoIcon from '../../assets/icons/info_icon.svg';
 
-function AudioFeatureBar() {
+function AudioFeatureBar({ feature, value}) {
     return (
         <div className='audioFeatureBar'>
             <div className='audioFeatureBar__feature'>
-                <h3 className='audioFeatureBar__name'>Acousticness</h3>
+                <h3 className='audioFeatureBar__name'>{feature}</h3>
                 <img className='audioFeatureBar__info' src={infoIcon} alt="Info Icon" />
             </div>
-            <ProgressBar completed={60} isLabelVisible={false} baseBgColor={'#1db95466'} bgColor={'#1DB954'}/>
+            <ProgressBar completed={value * 100} isLabelVisible={false} baseBgColor={'#1db95466'} bgColor={'#1DB954'}/>
             <div className='audioFeatureBar__range'>
                 <p className='audioFeatureBar__value'>0</p>
                 <p className='audioFeatureBar__value'>1</p>
--- src/pages/Analysis/Analysis.jsx
+++ src/pages/Analysis/Analysis.jsx
@@ -6,15 +6,28 @@ const URL = 'http://localhost:8080';
 
 function Analysis() {
     const [genres, setGenres] = useState(null);
+    const [selectedGenre, setSelectedGenre] = useState(null);
+    const [audioFeatures, setAudioFeatures] = useState(null);
 
     useEffect(() => {
         axios.get(URL + '/genres').then((response) => {
             setGenres(response.data);
         }).catch((error) => {
             console.log(`Error retrieving genres: ${error}`);
-        })
+        });
 
-    }, []);
+        if (selectedGenre) {
+            const genre = genres.filter((genre) => {
+                return genre.genre_name === selectedGenre;
+            })
+            console.log(genre);
+            axios.get(`${URL}/genres/${genre[0].id}/audio-features`).then((response) => {
+                console.log(response);
+                setAudioFeatures(response.data);
+            })
+        }
+
+    }, [selectedGenre]);
 
     if (!genres) {
         return (
@@ -24,19 +37,21 @@ function Analysis() {
 
     return (
         <section className='analysis'>
-            <select className='analysis__selector' name="genre" id="genre">
-                <option value="">Select Genre</option>
+            <select className='analysis__selector' onChange={(e) => { setSelectedGenre(e.target.value) }} name="genre" id="genre">
+                {!audioFeatures ? <option value="">Select Genre</option> : ''}
                 {genres.map((genre) => {
-                    return <option value={genre.genre_name}>{genre.genre_name}</option>
+                    return <option key={genre.id} value={genre.genre_name}>{genre.genre_name}</option>
                 })}
             </select>
-            <AudioFeatureBar/>
-            <AudioFeatureBar/>
-            <AudioFeatureBar/>
-            <AudioFeatureBar/>
-            <AudioFeatureBar/>
-            <AudioFeatureBar/>
-            <AudioFeatureBar/>
+                <>
+                    <AudioFeatureBar feature={'Acousticness'} value={audioFeatures ? audioFeatures[0].acousticness: 0}/>
+                    <AudioFeatureBar feature={'Danceability'} value={audioFeatures ? audioFeatures[0].danceability: 0}/>
+                    <AudioFeatureBar feature={'Valence'} value={audioFeatures ? audioFeatures[0].valence: 0}/>
+                    <AudioFeatureBar feature={'Energy'} value={audioFeatures ? audioFeatures[0].energy: 0}/>
+                    <AudioFeatureBar feature={'Instrumentalness'} value={audioFeatures ? audioFeatures[0].instrumentalness: 0}/>
+                    <AudioFeatureBar feature={'Liveness'} value={audioFeatures ? audioFeatures[0].liveness: 0}/>
+                    <AudioFeatureBar feature={'Speechiness'} value={audioFeatures ? audioFeatures[0].speechiness: 0}/>
+                </>
         </section>
     );
 
