66ae2c8a404655d9c8202c008fe4987f55801179
Troy-Smith-95 <trsmith.runner@gmail.com>
2022-12-10 16:55:33 -0500

    add in section for other audio feature data, add resposiveness accross different screen sizes to elements

--- src/pages/Analysis/Analysis.jsx
+++ src/pages/Analysis/Analysis.jsx
@@ -2,12 +2,14 @@ import './Analysis.scss';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import AudioFeatureBar from '../../Components/AudioFeatureBar/AudioFeatureBar';
+import infoIcon from '../../assets/icons/info_icon.svg';
 const URL = 'http://localhost:8080';
 
 function Analysis() {
     const [genres, setGenres] = useState(null);
     const [selectedGenre, setSelectedGenre] = useState(null);
     const [audioFeatures, setAudioFeatures] = useState(null);
+    const [key, setKey] = useState(null);
 
     useEffect(() => {
         axios.get(URL + '/genres').then((response) => {
@@ -23,11 +25,42 @@ function Analysis() {
             console.log(genre);
             axios.get(`${URL}/genres/${genre[0].id}/audio-features`).then((response) => {
                 console.log(response);
+                determineKey(response.data[0].key);
                 setAudioFeatures(response.data);
             })
         }
 
     }, [selectedGenre]);
+
+    function determineKey(keyValue) {
+        if (keyValue === 0) {
+            setKey('C');
+        } else if (keyValue === 1) {
+            setKey('C♯/D♭');
+        } else if (keyValue === 2) {
+            setKey('D');
+        } else if (keyValue === 3) {
+            setKey('D♯/E♭');
+        } else if (keyValue === 4) {
+            setKey('E');
+        } else if (keyValue === 5) {
+            setKey('F/E♯');
+        } else if (keyValue === 6) {
+            setKey('F♯/G♭');
+        } else if (keyValue === 7) {
+            setKey('G');
+        } else if (keyValue === 8) {
+            setKey('G♯/A♭');
+        } else if (keyValue === 9) {
+            setKey('A');
+        } else if (keyValue === 10) {
+            setKey('A♯/B♭');
+        } else if (keyValue === 11) {
+            setKey('B');
+        } else {
+            setKey('-');
+        }
+    }
 
     if (!genres) {
         return (
@@ -43,15 +76,45 @@ function Analysis() {
                     return <option key={genre.id} value={genre.genre_name}>{genre.genre_name}</option>
                 })}
             </select>
-                <>
-                    <AudioFeatureBar feature={'Acousticness'} value={audioFeatures ? audioFeatures[0].acousticness: 0}/>
-                    <AudioFeatureBar feature={'Danceability'} value={audioFeatures ? audioFeatures[0].danceability: 0}/>
-                    <AudioFeatureBar feature={'Valence'} value={audioFeatures ? audioFeatures[0].valence: 0}/>
-                    <AudioFeatureBar feature={'Energy'} value={audioFeatures ? audioFeatures[0].energy: 0}/>
-                    <AudioFeatureBar feature={'Instrumentalness'} value={audioFeatures ? audioFeatures[0].instrumentalness: 0}/>
-                    <AudioFeatureBar feature={'Liveness'} value={audioFeatures ? audioFeatures[0].liveness: 0}/>
-                    <AudioFeatureBar feature={'Speechiness'} value={audioFeatures ? audioFeatures[0].speechiness: 0}/>
-                </>
+            <div className='analysis__insights'>
+                <div className='analysis__progressBars'>
+                    <AudioFeatureBar feature={'Acousticness'} value={audioFeatures ? audioFeatures[0].acousticness : 0} />
+                    <AudioFeatureBar feature={'Danceability'} value={audioFeatures ? audioFeatures[0].danceability : 0} />
+                    <AudioFeatureBar feature={'Valence'} value={audioFeatures ? audioFeatures[0].valence : 0} />
+                    <AudioFeatureBar feature={'Energy'} value={audioFeatures ? audioFeatures[0].energy : 0} />
+                    <AudioFeatureBar feature={'Instrumentalness'} value={audioFeatures ? audioFeatures[0].instrumentalness : 0} />
+                    <AudioFeatureBar feature={'Liveness'} value={audioFeatures ? audioFeatures[0].liveness : 0} />
+                    <AudioFeatureBar feature={'Speechiness'} value={audioFeatures ? audioFeatures[0].speechiness : 0} />
+                </div>
+                <div className='analysis__otherData'>
+                    <img className='analysis__infoIcon' src={infoIcon} alt="Info Icon" />
+                    <div className='analysis__data'>
+                        <h3 className='analysis__feature'>Duration</h3>
+                        <p className='analysis__value'>{audioFeatures ? `${Math.floor(audioFeatures[0].duration_ms / 1000 / 60)}m ${Math.floor((audioFeatures[0].duration_ms / 1000) % 60)}s` : '-'}</p>
+                    </div>
+                    <div className='analysis__data'>
+                        <h3 className='analysis__feature'>Time Signature</h3>
+                        <p className='analysis__value'>{audioFeatures ? `${audioFeatures[0].time_signature}/4` : '-'}</p>
+                    </div>
+                    <div className='analysis__data'>
+                        <h3 className='analysis__feature'>Modality</h3>
+                        <p className='analysis__value'>{audioFeatures ? `${audioFeatures[0].mode === 0 ? 'Minor' : 'Major'}` : '-'}</p>
+                    </div>
+                    <div className='analysis__data'>
+                        <h3 className='analysis__feature'>Tempo</h3>
+                        <p className='analysis__value'>{audioFeatures ? audioFeatures[0].tempo.toFixed(2) : '-'}</p>
+                    </div>
+                    <div className='analysis__data'>
+                        <h3 className='analysis__feature'>Loudness</h3>
+                        <p className='analysis__value'>{audioFeatures ? audioFeatures[0].loudness.toFixed(2) : '-'}</p>
+                    </div>
+                    <div className='analysis__data'>
+                        <h3 className='analysis__feature'>Key</h3>
+                        <p className='analysis__value'>{audioFeatures ? key : '-'}</p>
+                    </div>
+
+                </div>
+            </div>
         </section>
     );
 
