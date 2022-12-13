713f7b12ac7bab405e33f26c0ba43c28c9f7d94a
Troy-Smith-95 <trsmith.runner@gmail.com>
2022-12-13 13:58:58 -0500

    add animations to elements on landing page

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,37 +1,62 @@
 import './Features.scss';
+import Lottie from "lottie-react";
+import { motion } from "framer-motion";
 import playlistsGraphic from '../../assets/images/playlists_graphic.svg';
-import soundIcon from '../../assets/icons/sound_icon.svg';
-import chartIcon from '../../assets/icons/chart_icon.svg';
 import acousticness from '../../assets/images/acousticness.svg';
 import danceability from '../../assets/images/danceability.svg';
 import instrumentalness from '../../assets/images/instrumentalness.svg';
-import inspirationIcon from '../../assets/icons/inspiration_icon.svg';
 import playlist1Icon from '../../assets/images/playlist1_icon.png';
 import playlist2Icon from '../../assets/images/playlist2_icon.png';
 import playlist3Icon from '../../assets/images/playlist3_icon.png';
 import playlist4Icon from '../../assets/images/playlist4_icon.png';
+import music from '../../assets/lotties/lf30_editor_kdlinqr7.json';
+import graph from '../../assets/lotties/lf30_editor_tpwusmvv.json';
+import lightbulb from '../../assets/lotties/lf30_editor_phoyp9bp.json';
+
 
 function Features() {
+    const style = {
+        height: 100,
+    };
+
+    const Variant = {
+        visible: {
+            opacity: 1, scale: 1,
+            transition: {
+                staggerChildren: 0.5
+            }
+        },
+        hidden: { opacity: 0, scale: 0 },
+    }
+    const item = {
+        hidden: { opacity: 0, x:-100 },
+        visible: { opacity: 1, x: 0, transition: { duration: 1} }
+    }
+    const itemTwo = {
+        hidden: { opacity: 0, x:100 },
+        visible: { opacity: 1, x: 0, transition: { duration: 1} }
+    }
+
     return (
         <section className='features'>
             <div className='features__feature'>
-                <img className='features__graphic' src={playlistsGraphic} alt="" />
+                <motion.img  whileHover={{ scale: [null, 1.3, 1.2] }} transition={{ duration: 0.3 }} className='features__graphic' src={playlistsGraphic} alt="" />
                 <div className='features__container'>
-                    <img className='features__icon' src={soundIcon} alt="" />
+                    <Lottie style={style} animationData={music} />
                     <h2 className='features__title'>See how taste makers evolve</h2>
                     <p className='features__text'>We generate our insights through tracking Spotify’s premier editorial playlists so you can stay on top of the sounds that catch their ear.</p>
                 </div>
             </div>
             <div className='features__feature'>
                 <div className='features__container'>
-                    <img className='features__icon' src={chartIcon} alt="" />
+                    <Lottie style={style} animationData={graph} />
                     <h2 className='features__title'>Visualize and compare data</h2>
                     <p className='features__text'>Our dashboard makes it easy to see the changes in the sounds of genres and make comparisons to other genres, artists, songs or playlists.</p>
                 </div>
                 <div className='features__graphic features__graphic--flexStart'>
-                    <img className='features__dataGraphic features__dataGraphic--one' src={acousticness} alt="acousticness" />
-                    <img className='features__dataGraphic features__dataGraphic--two' src={danceability} alt="danceability" />
-                    <img className='features__dataGraphic features__dataGraphic--three' src={instrumentalness} alt="instrumentalness" />
+                    <motion.img variants={item} initial='hidden' whileInView='visible' className='features__dataGraphic features__dataGraphic--one' src={acousticness} alt="acousticness" />
+                    <motion.img variants={itemTwo} initial='hidden' whileInView='visible'  className='features__dataGraphic features__dataGraphic--two' src={danceability} alt="danceability" />
+                    <motion.img variants={item} initial='hidden' whileInView='visible'  className='features__dataGraphic features__dataGraphic--three' src={instrumentalness} alt="instrumentalness" />
                 </div>
             </div>
             <div className='features__feature'>
@@ -42,7 +67,7 @@ function Features() {
                     <img className='features__playlistGraphic' src={playlist4Icon} alt="instrumentalness" />
                 </div>
                 <div className='features__container'>
-                    <img className='features__icon' src={inspirationIcon} alt="" />
+                    <Lottie style={style} animationData={lightbulb} />
                     <h2 className='features__title'>Get inspiration for the music you want to make</h2>
                     <p className='features__text'>Trax can generate playlists and save them right to your Spotify based on a specific sound profile to help give you some inspiration for your next track.</p>
                 </div>
--- src/Components/WeeklyStats/WeeklyStats.jsx
+++ src/Components/WeeklyStats/WeeklyStats.jsx
@@ -1,34 +1,53 @@
 import './WeeklyStats.scss';
+import { motion } from "framer-motion";
 import playlistIcon from '../../assets/icons/playlist_icon.svg';
 import songIcon from '../../assets/icons/song_icon.svg';
 import dataIcon from '../../assets/icons/data_icon.svg';
 import genreIcon from '../../assets/icons/genre_icon.svg';
 
 function WeeklyStats() {
+
+    const statsVariant = {
+        visible: {
+            opacity: 1, scale: 1,
+            transition: {
+                staggerChildren: 0.5
+            }
+        },
+        hidden: { opacity: 0, scale: 0 },
+    }
+    const item = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1,
+            scale: [1, 1.5, 1],
+        }
+    }
     return (
         <section className='weeklyStats'>
             <h3 className='weeklyStats__title'>What we look at every week</h3>
-            <div className='weeklyStats__container'>
-                <div className='weeklyStats__statContainer'>
-                    <img className='weeklyStats__icon' src={playlistIcon} alt="Playlist Icon" />
-                    <p className='weeklyStats__stat'>100+</p>
-                    <p className='weeklyStats__label'>Playlists</p>
-                </div>
-                <div className='weeklyStats__statContainer'>
-                    <img className='weeklyStats__icon' src={songIcon} alt="Playlist Icon" />
-                    <p className='weeklyStats__stat'>5000+</p>
-                    <p className='weeklyStats__label'>Songs</p>
-                </div>
-                <div className='weeklyStats__statContainer'>
-                    <img className='weeklyStats__icon' src={dataIcon} alt="Playlist Icon" />
-                    <p className='weeklyStats__stat'>25K+</p>
-                    <p className='weeklyStats__label'>Data Points</p>
-                </div>
-                <div className='weeklyStats__statContainer'>
-                    <img className='weeklyStats__icon' src={genreIcon} alt="Playlist Icon" />
-                    <p className='weeklyStats__stat'>15</p>
-                    <p className='weeklyStats__label'>Genres</p>
-                </div>
+            <div className='weeklyStats__box'>
+                <motion.div variants={statsVariant} initial='hidden' whileInView='visible' className='weeklyStats__container'>
+                    <motion.div variants={item} className='weeklyStats__statContainer'>
+                        <img className='weeklyStats__icon' src={playlistIcon} alt="Playlist Icon" />
+                        <p className='weeklyStats__stat'>100+</p>
+                        <p className='weeklyStats__label'>Playlists</p>
+                    </motion.div>
+                    <motion.div variants={item} className='weeklyStats__statContainer'>
+                        <img className='weeklyStats__icon' src={songIcon} alt="Playlist Icon" />
+                        <p className='weeklyStats__stat'>5000+</p>
+                        <p className='weeklyStats__label'>Songs</p>
+                    </motion.div>
+                    <motion.div variants={item} className='weeklyStats__statContainer'>
+                        <img className='weeklyStats__icon' src={dataIcon} alt="Playlist Icon" />
+                        <p className='weeklyStats__stat'>25K+</p>
+                        <p className='weeklyStats__label'>Data Points</p>
+                    </motion.div>
+                    <motion.div variants={item} className='weeklyStats__statContainer'>
+                        <img className='weeklyStats__icon' src={genreIcon} alt="Playlist Icon" />
+                        <p className='weeklyStats__stat'>15</p>
+                        <p className='weeklyStats__label'>Genres</p>
+                    </motion.div>
+                </motion.div>
             </div>
         </section>
     );
